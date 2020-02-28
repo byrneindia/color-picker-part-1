86e90f2da01a28c2563ae57ae68c076ab5487738
India Byrne <byrneindia@gmail.com>
2020-02-28 13:25:53 -0700

    Push.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-window.onload = funtion () {
+window.onload = function () {
 	initColorPicker();
-};
+}
 function initColorPicker() {
 	let colorBox =document.getElementByd("color box");
 	let rgb = {
@@ -8,18 +8,18 @@ function initColorPicker() {
 		green: document.getElementById("green"),
 		blue: document.getElementById("blue"),
 	};
-let colorPickers=document.getElementsByClassName("picker");
-); setColorPickerEventListeners(colorBox, rgb, colorPickers);
+	let colorPickers=document.getElementsByClassName("picker");
+	setColorPickerEventListeners(colorBox, rgb, colorPickers);
 }
 function setColorPickerEventListeners(colorBox, rgb, pictureElements) {
 	let pickerLen = pickerElements.lenght;
 	for(let i = 0; i < pickerelements.length; i++) {
 		pickerelements [i]. addEventListener('change', () => {
-			let red = colors.red.value
-			let green = colors.green.value
-			let blue= colors.blue.value
+			let red = colors.red.value;
+			let green = colors.green.value;
+			let blue= colors.blue.value;
 			setBoxBGColor(element, red, green, blue);
-			setDisplayValues (red, green, blue)
+			setDisplayValues (red, green, blue);
 		});
 	}
 }
@@ -28,12 +28,12 @@ function setBoxBGColor(colorBox, red, green, blue) {
 	colorBox.style.backgroundColor = "rgb(" + rgbVal + ")";
 }
 
-funtion setElementBGColor (bgElement, red, green, blue){
+function setElementBGColor (bgElement, red, green, blue) {
 	let redVal = document.getElementById("redVal");
 	let greenVal = document.getElementById("greebVal");
 	let blueVal = document.getElementById("blueVal");
-	setDisplayValues(red, green, blue)
-
+	setDisplayValues(red, green, blue);
 	redVal.innerText = red;
 	greenVal.innertext = green;
-	blueVal. innerText = blue;
+	blueVal.innerText = blue;
+}
