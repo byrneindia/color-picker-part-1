fa175ab991d4cfc8982280324e4b809d7f1e5206
India Byrne <byrneindia@gmail.com>
2020-02-28 12:00:12 -0700

    Push.

---
+++ script.js
@@ -0,0 +1,39 @@
+window.onload = funtion () {
+	initColorPicker();
+};
+function initColorPicker() {
+	let colorBox =document.getElementByd("color box");
+	let rgb = {
+		red: document.getElementById("red"),
+		green: document.getElementById("green"),
+		blue: document.getElementById("blue"),
+	};
+let colorPickers=document.getElementsByClassName("picker");
+); setColorPickerEventListeners(colorBox, rgb, colorPickers);
+}
+function setColorPickerEventListeners(colorBox, rgb, pictureElements) {
+	let pickerLen = pickerElements.lenght;
+	for(let i = 0; i < pickerelements.length; i++) {
+		pickerelements [i]. addEventListener('change', () => {
+			let red = colors.red.value
+			let green = colors.green.value
+			let blue= colors.blue.value
+			setBoxBGColor(element, red, green, blue);
+			setDisplayValues (red, green, blue)
+		});
+	}
+}
+function setBoxBGColor(colorBox, red, green, blue) {
+	rbgVal = [red, green, blue].join(',');
+	colorBox.style.backgroundColor = "rgb(" + rgbVal + ")";
+}
+
+funtion setElementBGColor (bgElement, red, green, blue){
+	let redVal = document.getElementById("redVal");
+	let greenVal = document.getElementById("greebVal");
+	let blueVal = document.getElementById("blueVal");
+	setDisplayValues(red, green, blue)
+
+	redVal.innerText = red;
+	greenVal.innertext = green;
+	blueVal. innerText = blue;
